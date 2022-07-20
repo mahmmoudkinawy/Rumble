a4acc132485b9e8cb5f0ef8b749133c49db34f63
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-07-20 15:14:50 +0200

    Added navbar component and it's styles

--- rumble-client/src/app/app.module.ts
+++ rumble-client/src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
