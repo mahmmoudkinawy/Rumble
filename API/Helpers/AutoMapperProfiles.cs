bd15861dd426a82eed2644dec82c30464999a21a
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-07-24 16:29:48 +0200

    Fixed the missing values and added photo url to return by the member dto

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -3,7 +3,9 @@ public class AutoMapperProfiles : Profile
 {
     public AutoMapperProfiles()
     {
-        CreateMap<UserEntity, MemberDto>();
+        CreateMap<UserEntity, MemberDto>()
+            .ForMember(d => d.Age, o => o.MapFrom(u => u.DateOfBirth.CalculateAge()))
+            .ForMember(d => d.PhotoUrl, o => o.MapFrom(u => u.Photos.FirstOrDefault(p => p.IsMain).Url));
         CreateMap<PhotoEntity, PhotoDto>();
     }
 }
