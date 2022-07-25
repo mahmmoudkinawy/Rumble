3603a98a4c01d61115daf63a398fffde3b90207e
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-07-25 13:56:58 +0200

    Adde member types and members services

--- rumble-client/src/app/pages/members/member-list/member-list.component.ts
+++ rumble-client/src/app/pages/members/member-list/member-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Member } from 'src/app/models/member';
+import { MembersService } from 'src/app/services/members.service';
+
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.scss']
+  styleUrls: ['./member-list.component.scss'],
 })
 export class MemberListComponent implements OnInit {
+  members: Member[] = [];
 
-  constructor() { }
+  constructor(private membersService: MembersService) {}
 
   ngOnInit(): void {
+    this.loadMembers();
   }
 
+  loadMembers() {
+    this.membersService
+      .getMembers()
+      .subscribe((members) => (this.members = members));
+  }
 }
