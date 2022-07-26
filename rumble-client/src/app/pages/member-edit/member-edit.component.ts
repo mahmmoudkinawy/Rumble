a12c0dec2a87d5a969e3da20f6b81713724802c0
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-07-26 18:23:10 +0200

    Added member edit component but not it's funcationliaty

--- rumble-client/src/app/pages/member-edit/member-edit.component.ts
+++ rumble-client/src/app/pages/member-edit/member-edit.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+
+import { Member } from 'src/app/models/member';
+import { User } from 'src/app/models/user';
+import { AccountService } from 'src/app/services/account.service';
+import { MembersService } from 'src/app/services/members.service';
 
 @Component({
   selector: 'app-member-edit',
   templateUrl: './member-edit.component.html',
-  styleUrls: ['./member-edit.component.scss']
+  styleUrls: ['./member-edit.component.scss'],
 })
 export class MemberEditComponent implements OnInit {
+  user: User | null = null;
+  member: Member | null = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private accountService: AccountService,
+    private membersService: MembersService
+  ) {
+    this.accountService.currentUser$
+      .pipe(take(1))
+      .subscribe((user) => (this.user = user));
   }
 
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    this.membersService
+      .getMember(this.user!.username)
+      .subscribe((member) => (this.member = member));
+  }
+
+  updateMember() {
+    console.log(this.member);
+  }
 }
