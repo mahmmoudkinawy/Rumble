139ed1df93b199defdfed904441812f42b2bb32d
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-07-27 21:58:20 +0200

    Added local statemangment instead of Redux or MobX

--- rumble-client/src/app/pages/members/member-list/member-list.component.ts
+++ rumble-client/src/app/pages/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Member } from 'src/app/models/member';
 import { MembersService } from 'src/app/services/members.service';
@@ -9,17 +10,11 @@ import { MembersService } from 'src/app/services/members.service';
   styleUrls: ['./member-list.component.scss'],
 })
 export class MemberListComponent implements OnInit {
-  members: Member[] = [];
+  members$: Observable<Member[]> | null = null;
 
   constructor(private membersService: MembersService) {}
 
   ngOnInit(): void {
-    this.loadMembers();
-  }
-
-  loadMembers() {
-    this.membersService
-      .getMembers()
-      .subscribe((members) => (this.members = members));
+    this.members$ = this.membersService.getMembers();
   }
 }
--- rumble-client/src/app/services/members.service.ts
+++ rumble-client/src/app/services/members.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 import { Member } from '../models/member';
@@ -8,19 +9,33 @@ import { Member } from '../models/member';
   providedIn: 'root',
 })
 export class MembersService {
+  members: Member[] = [];
   private readonly baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   getMembers() {
-    return this.http.get<Member[]>(`${this.baseUrl}/users`);
+    if (this.members.length > 0) return of<Member[]>(this.members);
+    return this.http.get<Member[]>(`${this.baseUrl}/users`).pipe(
+      map((members) => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find((m) => m.username === username);
+    if (member !== undefined) return of<Member>(member);
     return this.http.get<Member>(`${this.baseUrl}/users/${username}`);
   }
 
   updateMember(member: Member) {
-    return this.http.put(`${this.baseUrl}/users`, member);
+    return this.http.put(`${this.baseUrl}/users`, member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
   }
 }
