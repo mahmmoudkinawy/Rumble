f4f53083c162f5d36c13fc95b6703ee5ae2b2984
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-08-08 15:36:07 +0200

    Added pagination meta data for headers

---
+++ API/Extensions/HttpExtenstions.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Extensions;
+public static class HttpExtenstions
+{
+    public static void AddPaginationHeader(this HttpResponse response,
+        int currentPage,
+        int itemsPerPage,
+        int totalPages,
+        int totalItems)
+    {
+        var paginationHeader = new PaginationHeader(currentPage,
+            itemsPerPage,
+            totalPages,
+            totalItems);
+
+        response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
+        response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+    }
+}
