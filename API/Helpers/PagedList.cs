34e6f3e6fceeccfb3fef97d47e2302f5393b7754
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-08-08 14:07:26 +0200

    Added aall the funcationloty for adding pagination

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -3,31 +3,32 @@ public class PagedList<T> : List<T>
 {
     public int CurrentPage { get; set; }
     public int TotalPages { get; set; }
-    public int PageSize { get; set; }
     public int TotalCount { get; set; }
+    public int PageSize { get; set; }
 
     public PagedList(
         IEnumerable<T> items,
         int pageNumber,
-        int pageSize,
-        int count)
+        int count,
+        int pageSize)
     {
         CurrentPage = pageNumber;
         TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-        PageSize = pageSize;
         TotalCount = count;
+        PageSize = pageSize;
 
         AddRange(items);
     }
 
-    public static async Task<PagedList<T>> CreateAsync(
+    public async Task<PagedList<T>> CreateAsync(
         IQueryable<T> source,
         int pageNumber,
         int pageSize)
     {
         var count = await source.CountAsync();
         var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
-    
-        return new PagedList<T>(items, pageNumber, pageSize, count);
+
+        return new PagedList<T>(items, pageNumber, count, pageSize);
     }
+
 }
---
+++ API/Helpers/UserParams.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Helpers;
+public class UserParams
+{
+    private const int _maxPageSize = 50;
+    public int PageNumber { get; init; } = 1;
+    public int _pageSize { get; set; }
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > _maxPageSize ? _maxPageSize : value;
+    }
+}
