83b9b080afbbd869825fa46f82a784953aee59cb
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-08-21 19:54:56 +0200

    Added message type and get message end point to the service

--- rumble-client/src/app/pages/messages/messages.component.ts
+++ rumble-client/src/app/pages/messages/messages.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MessageService } from 'src/app/services/message.service';
+import { Message } from 'src/app/models/message';
+import { Pagination } from 'src/app/models/pagination';
+
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+  styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[] | null = [];
+  pagination: Pagination | null = null;
+  pageNumber = 1;
+  pageSize = 5;
+  container = 'Unread';
 
-  constructor() { }
+  constructor(private messageService: MessageService) {}
 
   ngOnInit(): void {
+    this.loadMessages();
   }
 
+  loadMessages() {
+    this.messageService
+      .getMessages(this.pageNumber, this.pageSize, this.container)
+      .subscribe((response) => {
+        this.messages = response.result;
+        this.pagination = response.pagination;
+      });
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.page;
+    this.loadMessages();
+  }
 }
--- rumble-client/src/app/services/message.service.ts
+++ rumble-client/src/app/services/message.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+import { getPaginatedResult, getPaginationHeaders } from './pagination-helper';
+import { Message } from '../models/message';
 
 @Injectable({
   providedIn: 'root',
@@ -7,8 +11,13 @@ import { Injectable } from '@angular/core';
 export class MessageService {
   constructor(private http: HttpClient) {}
 
-  getMessages(){
-    
+  getMessages(pageNumber: number, pageSize: number, container: string) {
+    let params = getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('Container', container);
+    return getPaginatedResult<Message[]>(
+      `${environment.apiUrl}/messages`,
+      params,
+      this.http
+    );
   }
-
 }
